229d42318526e6ac1283a47cd32b86d55903d246
Scorpioeric-dev <ericscorpio1@gmail.com>
2021-01-21 20:46:11 -0800

    footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
+import { Footer } from "./Components/Footer";
 import { Header } from "./Components/Header";
 import { Home } from "./Components/Home";
-
-
 
 function App() {
   return (
     <div className="app">
-    <Header/>
+      <Header />
       <Home />
+      <Footer />
       {/*Header*/}
       {/*Banner*/}
       {/*Card*/}
