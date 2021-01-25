d1f669706ab77042352ed7680dcfa59bca6d619a
Scorpioeric-dev <ericscorpio1@gmail.com>
2021-01-25 07:51:02 -0800

    search-routes

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -5,8 +5,10 @@ import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { Button } from "@material-ui/core";
 import PeopleIcon from "@material-ui/icons/People";
+import { useHistory } from "react-router-dom";
 
 export const Search = () => {
+  const history = useHistory();
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
 
@@ -30,7 +32,7 @@ export const Search = () => {
         <PeopleIcon />
       </h2>
       <input min={0} defaultValue={2} type="number" />
-      <Button>
+      <Button onClick={() => history.push("/search")}>
         <strong>Search Airbnb</strong>
       </Button>
     </div>
