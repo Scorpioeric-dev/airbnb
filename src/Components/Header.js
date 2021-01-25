bf71dd482a61f824ccedb814e175aba7138b2c5a
Scorpioeric-dev <ericscorpio1@gmail.com>
2021-01-25 07:52:59 -0800

    link

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,16 +4,18 @@ import SearchIcon from "@material-ui/icons/Search";
 import LanguageIcon from "@material-ui/icons/Language";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { Avatar } from "@material-ui/core";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
     <div className="header">
-      <img
-        className="header_icon"
-        src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
-        alt=""
-      />
+      <Link to="/">
+        <img
+          className="header_icon"
+          src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+          alt=""
+        />
+      </Link>
 
       <div className="header_center">
         <input type="text" />
@@ -21,7 +23,9 @@ export const Header = () => {
       </div>
 
       <div className="header_right">
-        <p><strong>Become a Host</strong></p>
+        <p>
+          <strong>Become a Host</strong>
+        </p>
         <LanguageIcon />
         <ExpandMoreIcon />
         <Avatar />
